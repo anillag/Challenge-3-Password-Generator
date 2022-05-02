40b193c5a2a93b1e14424402a4feb8cfd8ef22a6
Anillag <101154249+anillag@users.noreply.github.com>
2022-05-01 23:10:58 -0400

    Added pseudocode to script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 
+// **********************
+// ** Pseudocode Start **
+// **********************
+// User clicks button to begin password prompt sequence
+// User prompts take input to select criteria
+// User prompted for password length between 8-128
+// User prompted for lowercase letters, y/n
+// User prompted for uppercase letters, y/n
+// User prompted for numerical values, y/n
+// User prompted for special characters, y/n
+// Validate all user input as it's being collected
+// Validate that at least one character type was selected
+// Generate password based on selected criteria
+// Display password in either window.alert() or replace HTML
+// **********************
+// *** Pseudocode End ***
+// **********************
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
