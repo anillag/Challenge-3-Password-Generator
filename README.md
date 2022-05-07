1a7f70bdcd731e6cb05fef9206f3a2ea5a4fc1bc
Anillag <101154249+anillag@users.noreply.github.com>
2022-05-07 15:22:44 -0400

    Finalized script.js, updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Challenge-3-Password-Generator
+
+## Website Purpose
+
+An intelligent random password generator that will take user inputs about the
+types of characters they wish to include, as well as the desired length of the
+password.
+
+## Page Built With
+
+- Semantic HTML 5
+- Basic CSS 3
+- JavaScript ES6 / ES2015
+
+## Usage
+
+Click the red Generate Password button to begin the password generation function.
+Follow the prompts to generate the desired password.
+
+![Screenshot of Password Generator webpage](Password-Generator_screenshot.png)
+
+## Website URL
+
+https://anillag.github.io/Challenge-3-Password-Generator/
+
+## Contributors
+
+Made with ❤️ by Joe Gallina (contains starter code from Trilogy Education Services)
+
+### ©️2022 Joe Gallina, Trilogy Education Services
--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 // Assignment code here
-var contentValidation = 0;
+// Setup empty strings as global variables, to be filled in later
 var pwLength = "";
 var pwLowercase = "";
 var pwUppercase = "";
 var pwNumerals = "";
 var pwSpChars = "";
+var finalPassword = "";
+var randomized = "";
+var notRandomized = [];
 
+var contentValidation = 0;
+
+// Setup filled strings as constants
 const valuesLowercase = "abcdefghijklmnopqrstuvwxyz";
 const valuesUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const valuesNumerals = "0123456789";
+const valuesNumerals = "01234567890";
 const valuesSpChars = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
 
-// **********************
-// ** Pseudocode Start **
-// **********************
+// Shuffle code
+const shuffle = str => [...str].sort(()=>Math.random()-.5).join('');
+
 // User clicks button to begin password prompt sequence
 function generatePassword() {
   // User prompted for password length between 8-128 - DONE
+  console.log("Welcome to the Password Generator!");
   var pwLength = window.prompt("Welcome to the Password Generator!  Please enter your desired password length, between 8 and 128");
   console.log("Password should be " + pwLength + " characters in length.");
   if (pwLength < 8 || pwLength > 128 || pwLength === "" || pwLength === null) {
     window.alert("Value selected is outside of the acceptable range or was left blank.  Please try again.")
     return generatePassword();
   }
-  // User prompted for lowercase letters, y/n
+  // User prompted for lowercase letters, Y/N
   var pwLowercase = window.prompt("Would you like lowercase letters included in your password?  [Y/N]");
   console.log(pwLowercase);
   pwLowercase = pwLowercase.toUpperCase();
@@ -32,7 +39,7 @@ function generatePassword() {
     window.alert("Entry was left blank.  Please try again.")
     return generatePassword();
   } 
-  // User prompted for uppercase letters, y/n
+  // User prompted for uppercase letters, Y/N
   var pwUppercase = window.prompt("Would you like uppercase letters included in your password?  [Y/N]");
   console.log(pwUppercase);
   pwUppercase = pwUppercase.toUpperCase();
@@ -41,7 +48,7 @@ function generatePassword() {
     window.alert("Entry was left blank.  Please try again.")
     return generatePassword();
   } 
-  // User prompted for numerical values, y/n
+  // User prompted for numerical values, Y/N
   var pwNumerals = window.prompt("Would you like numerals included in your password?  [Y/N]");
   console.log(pwNumerals);
   pwNumerals = pwNumerals.toUpperCase();
@@ -50,7 +57,7 @@ function generatePassword() {
     window.alert("Entry was left blank.  Please try again.")
     return generatePassword();
   } 
-  // User prompted for special characters, y/n
+  // User prompted for special characters, Y/N
   var pwSpChars = window.prompt("Would you like special characters included in your password?  [Y/N]");
   console.log(pwSpChars);
   pwSpChars = pwSpChars.toUpperCase();
@@ -60,7 +67,7 @@ function generatePassword() {
     return generatePassword();
   }
 
-  // Begin validation to check that at least ONE OR MORE options were selected ("Y")
+  // Begin validation to check that at least ONE OR MORE options were selected ("Y").  This section runs with no user input required.
   console.log('Beginning data validation to make sure at least one option was selected ("Y").')
   if (pwLowercase === "Y") {
     console.log("Password should contain lowercase letters.  Current 'contentValidation' value is " + contentValidation + ".  Incrementing 'contentValidation' variable by 1.");
@@ -91,26 +98,60 @@ function generatePassword() {
     console.log("Password should NOT contain special characters.  Skipping increment of 'contentValidation'.  Current 'contentValidation' value is " + contentValidation + ".");
   }
   
-  // Finalize validation that at least ONE OR MORE options was selected ("Y")
+  // Finalize validation that at least ONE OR MORE options was selected ("Y").  This validation step requires no user input.
   if (contentValidation > 0) {
-    console.log("The 'contentValidation' variable is set to " + contentValidation + ".  At least one option was selected.  Allowing script to continue.");
+    console.log("The 'contentValidation' variable is set to " + contentValidation + ".  This means at least one option was selected.  Allowing script to continue.");
   } else {
-    console.log("The 'contentValidation' variable is set to " + contentValidation + ".  No options were selected.  Restarting password generator.");
+    console.log("The 'contentValidation' variable is set to " + contentValidation + ".  This means no options were selected.  Restarting password generator.");
     window.alert("No options were selected.  Please try again.");
     return generatePassword();
   }
+
+  // Compile password based on selected criteria
+  if (pwLowercase === "Y") {
+    notRandomized += valuesLowercase;
+    console.log("Lowercase characters will be included.  Current array value is:  " + notRandomized);
+  } else {
+    console.log("Lowercase characters will not be included.  Current array value is:  " + notRandomized);
+  }
+
+  if (pwUppercase === "Y") {
+    notRandomized += valuesUppercase;
+    console.log("Uppercase characters will be included.  Current array value is:  " + notRandomized);
+  } else {
+    console.log("Uppercase characters will not be included.  Current array value is:  " + notRandomized);
+  }
+
+  if (pwNumerals === "Y") {
+    notRandomized += valuesNumerals;
+    console.log("Numerals will be included.  Current array value is:  " + notRandomized);
+  } else {
+    console.log("Numerals will not be included.  Current array value is:  " + notRandomized);
+  }
+
+  if (pwSpChars === "Y") {
+    notRandomized += valuesSpChars;
+    console.log("Special characters will be included.  Current array value is:  " + notRandomized);
+  } else {
+    console.log("Special characters will not be included.  Current array value is:  " + notRandomized);
+  }
+
+  // Randomize the collected characters
+  randomized = shuffle(notRandomized)
+
+  // Truncate the randomized characters
+  password = truncate(randomized, pwLength);
+
+  // Return the finalized password
+  return password;
 };
 
+// Generate password based on selected criteria - DONE-ish
+function truncate (string="", limit=0) {
+  return string.substring(0,limit)
+}
 
-// User prompts take input to select criteria - DONE
-// Validate all user input as it's being collected - DONE
-// Validate that at least one character type was selected - DONE
-// Generate password based on selected criteria
-// Display password in either window.alert() or replace HTML
-// Joseph's hint, use pop and/or push to add characters to the array for characters
-// **********************
-// *** Pseudocode End ***
-// **********************
+// Display password in either window.alert() or replace HTML - DONE
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
